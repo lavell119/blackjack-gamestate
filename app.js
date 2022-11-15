f2f189633a67f39b33795b7e7326e9284801f326
Lee <leewhiz119@gmail.com>
2022-11-14 20:46:55 -0500

    started working on gamestate

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const app = express()
+
+app.listen('3000', function(req,res){
+    console.log('listening on port...')
+})
+
---
+++ gamestate.js
@@ -0,0 +1,36 @@
+let gameState = {
+    //players
+    player1: {
+        score: 0,
+        win: null,
+        chips: 2000,
+        bust: false,
+        hand: {
+            card: null,
+            card: null,
+            card: null
+        }
+    },
+    player2: {
+        score: 0,
+        win: null,
+        chips: 2000,
+        bust: false,
+        hand: {
+            card: null,
+            card: null,
+            card: null
+        }
+    },
+    player3: {
+        score: 0,
+        win: null,
+        chips: 2000,
+        bust: false,
+        hand: {
+            card: null,
+            card: null,
+            card: null
+        }
+    }
+}
